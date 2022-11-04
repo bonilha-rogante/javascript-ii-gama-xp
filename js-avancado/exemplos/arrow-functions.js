c66e364d6d87f2a2636c0a336aba00bdeee6b545
Guilherme Bonilha <bonilha.rogante@gmail.com>
2022-11-04 15:42:32 -0300

    exercícios do curso de JS da GAMA

--- js-avancado/exemplos/arrow-functions.js
+++ js-avancado/exemplos/arrow-functions.js
@@ -1,15 +1,30 @@
-// function
+// // function
 
-function soma(a,b) {
-    return a + b
-}
+// function soma(a,b) {
+//     return a + b
+// }
 
-console.log(soma(3,5)) //8
+// console.log(soma(3,5)) //8
 
-//arrow function
+// //arrow function
+
+// const soma = (num1, num2) => num1 + num2
+
+// const sayHello = name => `Hello ${name}`
+
+// console.log(sayHello('Mari'))
+
+// function soma(a, b){
+//     return a + b
+// }
+
+// console.log(soma(3, 5))
+
+// Arrow function
 
 const soma = (num1, num2) => num1 + num2
+console.log(soma(7, 6))
 
-const sayHello = name => `Hello ${name}`
+const sayHello = name => `Hello, ${name}`
 
-console.log(sayHello('Mari'))
+console.log(sayHello('Guilherme'))
